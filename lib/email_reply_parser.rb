3ef3ecdb58bb84937193cf3b2ee3ce72bd42915f
Jakob Kragelund <jakob@ativo.dk>
2023-02-14 16:56:41 +0100

    Fix to danish gmail expression

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -142,7 +142,7 @@ class EmailReplyParser
 
       # Gmail in Danish
       /^\d{1,2}.*kl\..*skrev.*:$/,
-      /^Den (man|tir|ons|tor|fre|lør|søn)\. \d{1,2}.*kl\..*skrev.*:$/
+      /^Den (man|tir|ons|tor|fre|lør|søn)\. \d{1,2}.*kl\..*skrev.*$/
     ]
 
     ### Line-by-Line Parsing
